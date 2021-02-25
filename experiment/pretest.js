4c606d5dee02605b70c464ce97361372ff019d15
Virtual Labs IIT Delhi <71967438+vlabsdev-iitd@users.noreply.github.com>
2021-02-25 19:03:15 +0530

    Update pretest.js

--- experiment/pretest.js
+++ experiment/pretest.js
@@ -94,8 +94,8 @@
       question: "Q2. What would be behavior if the constructor has a return type?",
       answers: {
         a: "Compilation error",
-        b: "Runtime error"
-        c: "Compilation and runs successfully"
+        b: "Runtime error",
+        c: "Compilation and runs successfully",
         d: "Only String return type is allowed"
 
 
@@ -119,27 +119,20 @@
       question: "Q4. We can declare constructor as private?",
       answers: {
         a: "Yes",
-        b: "No",
+        b: "No"
       },
       correctAnswer: "a"
     },
 	
 	{
-      question: "Q5. Which of the following is/are true about constructors in Java?",
-      A) Constructor name should be same as class name.
-      B) If you dont define a constructor for a class, a default parameter less constructor is automatically created by the compiler. 
-      C) The default constructor calls super() and initializes all instance variables to default value like 0, null.
-      D) If we want to parent class constructor, it must be called in first line of constructor.
-
+      question: "Q5. Which of the following is/are true about constructors in Java? A) Constructor name should be same as class name. B) If you dont define a constructor for a class, a default parameter less constructor is automatically created by the compiler. C) The default constructor calls super() and initializes all instance variables to default value like 0, null. D) If we want to parent class constructor, it must be called in first line of constructor.",
 
       answers: {
         a: "1",
-        b: "1, 2"
-      },
-      c: "1, 2 and 3"
-      },
-      d: " 1, 2, 3 and 4"
-      },
+        b: "1, 2",
+      	c: "1, 2 and 3",
+      	d: " 1, 2, 3 and 4"
+     },
       correctAnswer: "b"
     },
   ];
