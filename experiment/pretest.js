1f64f1e2a16f0d43231afbf809e1e5c16f6edc1c
vlabsdev-iitd <vlabsdev.iitd@gmail.com>
2021-02-25 11:51:48 +0530

    Exp3

--- experiment/pretest.js
+++ experiment/pretest.js
@@ -1,135 +1,164 @@
- /////////////////////////////////////////////////////////////////////////////
 
- /////////////////////// Do not modify the below code ////////////////////////
+// Don't touch the below code
 
- /////////////////////////////////////////////////////////////////////////////
+(function() {
+  function buildQuiz() {
+    // we'll need a place to store the HTML output
+    const output = [];
 
- (function() {
-     function buildQuiz() {
-         // we'll need a place to store the HTML output
-         const output = [];
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // we'll want to store the list of answer choices
+      const answers = [];
 
-         // for each question...
-         myQuestions.forEach((currentQuestion, questionNumber) => {
-             // we'll want to store the list of answer choices
-             const answers = [];
+      // and for each available answer...
+      for (letter in currentQuestion.answers) {
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
 
-             // and for each available answer...
-             for (letter in currentQuestion.answers) {
-                 // ...add an HTML radio button
-                 answers.push(
-                     `<label>
-           <input type="radio" name="question${questionNumber}" value="${letter}">
-           ${letter} :
-           ${currentQuestion.answers[letter]}
-         </label>`
-                 );
-             }
+      // add this question and its answers to the output
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join("")} </div>`
+      );
+    });
 
-             // add this question and its answers to the output
-             output.push(
-                 `<div class="question"> ${currentQuestion.question} </div>
-       <div class="answers"> ${answers.join("")} </div>`
-             );
-         });
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join("");
+  }
 
-         // finally combine our output list into one string of HTML and put it on the page
-         quizContainer.innerHTML = output.join("");
-     }
+  function showResults() {
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll(".answers");
 
-     function showResults() {
-         // gather answer containers from our quiz
-         const answerContainers = quizContainer.querySelectorAll(".answers");
+    // keep track of user's answers
+    let numCorrect = 0;
 
-         // keep track of user's answers
-         let numCorrect = 0;
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-         // for each question...
-         myQuestions.forEach((currentQuestion, questionNumber) => {
-             // find selected answer
-             const answerContainer = answerContainers[questionNumber];
-             const selector = `input[name=question${questionNumber}]:checked`;
-             const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+      // if answer is correct
+      if (userAnswer === currentQuestion.correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
 
-             // if answer is correct
-             if (userAnswer === currentQuestion.correctAnswer) {
-                 // add to the number of correct answers
-                 numCorrect++;
+        // color the answers green
+        //answerContainers[questionNumber].style.color = "lightgreen";
+      } else {
+        // if answer is wrong or blank
+        // color the answers red
+        answerContainers[questionNumber].style.color = "red";
+      }
+    });
 
-                 // color the answers green
-                 //answerContainers[questionNumber].style.color = "lightgreen";
-             } else {
-                 // if answer is wrong or blank
-                 // color the answers red
-                 answerContainers[questionNumber].style.color = "red";
-             }
-         });
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
 
-         // show number of correct answers out of total
-         resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-     }
+  const quizContainer = document.getElementById("quiz");
+  const resultsContainer = document.getElementById("results");
+  const submitButton = document.getElementById("submit");
+  
 
-     const quizContainer = document.getElementById("quiz");
-     const resultsContainer = document.getElementById("results");
-     const submitButton = document.getElementById("submit");
-
-
-     /////////////////////////////////////////////////////////////////////////////
-
-     /////////////////////// Do not modify the above code ////////////////////////
-
-     /////////////////////////////////////////////////////////////////////////////
+// Don't touch the above code
 
 
 
 
+// Write your MCQs here --- Start --- --------------------
 
-
-     /////////////// Write the MCQ below in the exactly same described format ///////////////
-
-
-     const myQuestions = [{
-            question: "1.This is a sample question:", ///// Write the question inside double quotes
-            answers: {
-                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
-                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
-            },
-            correctAnswer: "a" ///// Write the correct option inside double quotes
-        },
-
+  const myQuestions = [
     {
-      question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
+      question: "Q1. What is false about constructor?",
       answers: {
-        a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
-        b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
-        c: "<img src='images/1c.PNG'>",      },
-      correctAnswer: "c"                ///// Write the correct option inside double quotes
+        a: " Constructors cannot be synchronized in Java",
+        b: " Java does not provide default copy constructor",
+        c: "Constructor can have a return type",
+        d: "“this” and “super” can be used in a constructor"
+      },
+      correctAnswer: "c"
     },
 
+    {
+      question: "Q2. What would be behavior if the constructor has a return type?",
+      answers: {
+        a: "Compilation error",
+        b: "Runtime error"
+        c: "Compilation and runs successfully"
+        d: "Only String return type is allowed"
 
 
-     ];
+
+      },
+      correctAnswer: "a"
+    },
+
+    {
+      question: "Q3. What is default accessibility modifier of a default constructor?",
+      answers: {
+        a: " Public",
+        b: "private",
+        c: "Same as class accessibility modifier",
+        d: "default"
+      },
+      correctAnswer: "c"
+    },
+	
+	{
+      question: "Q4. We can declare constructor as private?",
+      answers: {
+        a: "Yes",
+        b: "No",
+      },
+      correctAnswer: "a"
+    },
+	
+	{
+      question: "Q5. Which of the following is/are true about constructors in Java?",
+      A) Constructor name should be same as class name.
+      B) If you dont define a constructor for a class, a default parameter less constructor is automatically created by the compiler. 
+      C) The default constructor calls super() and initializes all instance variables to default value like 0, null.
+      D) If we want to parent class constructor, it must be called in first line of constructor.
+
+
+      answers: {
+        a: "1",
+        b: "1, 2"
+      },
+      c: "1, 2 and 3"
+      },
+      d: " 1, 2, 3 and 4"
+      },
+      correctAnswer: "b"
+    },
+  ];
 
 
 
 
-     /////////////////////////////////////////////////////////////////////////////
-
-     /////////////////////// Do not modify the below code ////////////////////////
-
-     /////////////////////////////////////////////////////////////////////////////
+// ---------------------------- End -------------------------------
 
 
-     // display quiz right away
-     buildQuiz();
-
-     // on submit, show results
-     submitButton.addEventListener("click", showResults);
- })();
 
 
- /////////////////////////////////////////////////////////////////////////////
 
- /////////////////////// Do not modify the above code ////////////////////////
 
- /////////////////////////////////////////////////////////////////////////////
+
+
+  // display quiz right away
+  buildQuiz();
+
+  // on submit, show results
+  submitButton.addEventListener("click", showResults);
+})();
---
+++ storyboard/mindmap/Constructor.mm
@@ -0,0 +1,90 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Constructor" FOLDED="false" ID="ID_1803259999" CREATED="1601292152201" MODIFIED="1601293108053" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Types" POSITION="right" ID="ID_1370963126" CREATED="1601292469302" MODIFIED="1601293108053" HGAP_QUANTITY="6.500000223517411 pt" VSHIFT_QUANTITY="-44.24999868124728 pt">
+<edge COLOR="#ff0000"/>
+<node TEXT="Default Constructor" ID="ID_1243593696" CREATED="1601292502077" MODIFIED="1601292621072" HGAP_QUANTITY="11.000000089406964 pt" VSHIFT_QUANTITY="-40.49999879300598 pt"/>
+<node TEXT="Parameterized constructor" ID="ID_1499533219" CREATED="1601292626865" MODIFIED="1601292665930"/>
+</node>
+<node TEXT="Characterstics" POSITION="left" ID="ID_1223534654" CREATED="1601292682320" MODIFIED="1601293100943" HGAP_QUANTITY="0.5000004023313398 pt" VSHIFT_QUANTITY="-41.999998748302495 pt">
+<edge COLOR="#0000ff"/>
+<node TEXT="cannot be private." ID="ID_1232121943" CREATED="1601292701685" MODIFIED="1601292738289" HGAP_QUANTITY="2.750000335276116 pt" VSHIFT_QUANTITY="-35.99999892711642 pt"/>
+<node TEXT="cannot return a value." ID="ID_1759346300" CREATED="1601292743343" MODIFIED="1601292768906" HGAP_QUANTITY="18.499999865889553 pt" VSHIFT_QUANTITY="-20.24999939650299 pt"/>
+<node TEXT="not have a return type" ID="ID_1870976820" CREATED="1601292773171" MODIFIED="1601292789609"/>
+</node>
+<node TEXT="Syntax" POSITION="right" ID="ID_1497957478" CREATED="1601292806162" MODIFIED="1601293105428" HGAP_QUANTITY="5.750000245869153 pt" VSHIFT_QUANTITY="50.9999984800816 pt">
+<edge COLOR="#00ff00"/>
+<node TEXT="Data Type" ID="ID_148125871" CREATED="1601292816497" MODIFIED="1601292843707"/>
+<node TEXT="Variable" ID="ID_1285443993" CREATED="1601292851555" MODIFIED="1601292868454"/>
+<node TEXT="Methods" ID="ID_1020868786" CREATED="1601292875634" MODIFIED="1601292890806"/>
+</node>
+<node TEXT="Advantage" POSITION="left" ID="ID_318811219" CREATED="1601292984874" MODIFIED="1601293097662" HGAP_QUANTITY="-3.249999485909953 pt" VSHIFT_QUANTITY="44.999998658895535 pt">
+<edge COLOR="#ff00ff"/>
+<node TEXT=" eliminates placing the default values." ID="ID_734096739" CREATED="1601292995015" MODIFIED="1601293011038"/>
+<node TEXT="calling the normal or ordinary method implicitly." ID="ID_548995231" CREATED="1601293024259" MODIFIED="1601293042966"/>
+</node>
+</node>
+</map>
